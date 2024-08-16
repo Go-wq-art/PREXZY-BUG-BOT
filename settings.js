82394ed7b3e455466f45b647aecd6e335652ca10
Go-wq-art <feelyfvjam@gmail.com>
2024-08-16 19:54:52 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,8 +2,8 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "2349159895444"
-global.ownername = "Precious Ayomide👑"
+global.ownernomer = "27740245273"
+global.ownername = "Young James👑"
 global.ytname = "YT: Prexzyvilla"
 global.socialm = "GitHub: Prexzybooster"
 global.location = "Nigeria, Osun, Osogbo"
